d6614e68cc0a702b11858c9ac1d71b4c1da885ba
MooJi <mooji.hur@gmail.com>
2025-01-04 22:24:14 +0900

    [Feature] add c++ basics

---
+++ Section00/02-01-mainBasic/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#include <iostream>
+
+using namespace std;
+
+string inputWord;
+
+int main() {
+  cin >> inputWord;
+  cout << inputWord << endl;
+
+  return 0;
+}
---
+++ Section00/02-02-typedef/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef int myIntType;
+myIntType one = 1;
+
+int main(void) {
+  cout << one << endl;
+
+  return 0;
+}
