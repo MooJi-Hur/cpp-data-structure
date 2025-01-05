62a7817e190cd6f0f7d4661b8c9ed662a6130ad6
MooJi <mooji.hur@gmail.com>
2025-01-05 12:16:28 +0900

    [Feature] add chpater settings

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # cpp-data-structure
+- CMake 빌드를 이용한 IO 파일 관리
+
+### sh 파일 권한 부여
+```bash
+chmod +x setup-vscode.sh \
+    && chmod +x setup-problems.sh \
+    && chmod +x setup-chapter.sh
+```
+
+### 단원 폴더 만들기
+- `setup-chapter` 파일 실행
+- 섹션, 단원 이름 입력
+- 폴더 내 `main.cpp`, `input.txt`, `output.txt` 생성 확인
+
+- 예
+```bash
+./setup-chapter.sh section-00 02-01-main-basic
+```
+
+- 오류가 생길 경우, sh 파일 권한 확인
---
+++ setup-chapter.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Check if folder names are provided.
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <section_name> <chapter_name>"
+  exit 1
+fi
+
+# Define folder names.
+SECTION_NAME=$1
+CHAPTER_NAME=$2
+
+# Create folders.
+mkdir -p "$SECTION_NAME/$CHAPTER_NAME"
+
+# Change directory to chapter folder.
+cd "$SECTION_NAME/$CHAPTER_NAME"
+
+# Create the Main.cpp file
+touch main.cpp input.txt output.txt
+
+echo "Setup completed for $SECTION_NAME/$CHAPTER_NAME."
+
