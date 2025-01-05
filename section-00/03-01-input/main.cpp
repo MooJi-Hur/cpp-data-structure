fcb95779567eba62e74188a2be531bd7ec25b3cf
MooJi <mooji.hur@gmail.com>
2025-01-05 13:02:47 +0900

    [Feature] add 00-03 In

---
+++ section-00/03-01-input/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int inWord;
+
+int main() {
+  cin >> inWord;  // 개행 문자(띄어쓰기, 엔터) 직전까지 입력을 받음
+  scanf("%d", &inWord);  // 형식을 함께 정의
+
+  return 0;
+}
---
+++ section-00/03-02-cin/main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string inWord;
+
+int main() {
+  cin >> inWord;           // Hello, World!
+  cout << inWord << endl;  // Hello
+
+  return 0;
+}
