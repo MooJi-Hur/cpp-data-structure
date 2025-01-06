ae371886dea119cfa98f6c348f3214942a6d1a06
MooJi <mooji.hur@gmail.com>
2025-01-06 19:36:15 +0900

    [Feature] add 00-04-04 bool

---
+++ section-00/04-04-bool/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int myNumber = -1;
+
+  cout << bool(myNumber) << endl;
+
+  myNumber = 0;
+
+  cout << bool(myNumber) << endl;
+
+  myNumber = 3;
+
+  cout << bool(myNumber) << endl;
+
+  return 0;
+}
