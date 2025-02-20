286cf9c3e3c2c873d67ff8e1a6dcc9823fde22ae
MooJi <mooji.hur@gmail.com>
2025-02-20 09:14:26 +0900

    [Feature] update 01 D 10988

--- section-01/D-10988/main.cpp
+++ section-01/D-10988/main.cpp
@@ -3,36 +3,40 @@
  * URL: https://www.acmicpc.net/problem/10988
  */
 
+#include <algorithm>
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-string getInput()
-{
-    string myInput;
+int main() {
+  string inWord;
+  getline(cin >> ws, inWord);
 
-    cin >> myInput;
+  bool isPalindrom = true;
 
-    return myInput;
-}
-
-void printOutput(bool myOutput)
-{
-    cout << myOutput << '\n';
-}
-
-int main()
-{
-    bool isPalindrome = false;
-
-    string inWord = getInput();
-
-    cout << inWord << '\n';
-
-    isPalindrome = true;
-
-    printOutput(isPalindrome);
-
+  if (inWord.size() == 1) {
+    cout << isPalindrom;
     return 0;
+  }
+
+  size_t halfInWordSize = inWord.size() >> 1;
+
+  string forwardHalfWord = inWord.substr(0, halfInWordSize);
+
+  reverse(inWord.begin(), inWord.end());
+  string backwardHalfWord = inWord.substr(0, halfInWordSize);
+
+  for (int i = 0; i < halfInWordSize; ++i) {
+    bool isSameChar = (forwardHalfWord[i] == backwardHalfWord[i]);
+    if (!isSameChar) {
+      isPalindrom = false;
+      break;
+    }
+  }
+
+  cout << isPalindrom;
+
+  return 0;
 }
