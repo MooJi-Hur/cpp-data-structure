2594c63e5ce3c33d3bdcb602ccf85f5e80bbdba9
MooJi <mooji.hur@gmail.com>
2025-04-05 15:15:03 +0900

    [Feature] resolve Q 2636

--- section-02/Q-2636/main.cpp
+++ section-02/Q-2636/main.cpp
@@ -4,66 +4,74 @@
  */
 
 #include <iostream>
+#include <map>
+#include <set>
 #include <stack>
 #include <utility>
 #include <vector>
 
 using namespace std;
 
+constexpr int CHEESE = 1;
+constexpr int WALL = 0;
+
 constexpr int DIRECTIONS = 4;
 constexpr pair<int, int> DELTAS[DIRECTIONS] = {
     {-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 
-void readGrid(vector<vector<int>> &cheeseGrid) {
-  for (size_t rowIndex = 0; rowIndex < cheeseGrid.size(); ++rowIndex) {
-    for (size_t colIndex = 0; colIndex < cheeseGrid[0].size(); ++colIndex) {
-      bool isCheese;
-      cin >> isCheese;
-      cheeseGrid[rowIndex][colIndex] = isCheese;
+void readGrid(vector<vector<bool>> &inGrid) {
+  for (vector<bool> &row : inGrid) {
+    for (vector<bool>::reference cell : row) {
+      int inCell = 0;
+      cin >> inCell;
+      cell = (inCell == CHEESE);
     }
   }
 };
 
-void traceCheese(pair<int, int> &startNode,
-                 vector<vector<int>> &cheeseGrid,
-                 vector<vector<bool>> &visited,
-                 vector<int> &cheeseCounts,
-                 int &totalHours) {
+void findOpenNodes(vector<vector<bool>> &inGrid,
+                   int &hourCount,
+                   set<pair<int, int>> &openNodes,
+                   vector<vector<bool>> &visited,
+                   vector<vector<int>> &history) {
 
-  stack<pair<int, int>> dfsQueue;
-  dfsQueue.push(startNode);
+  pair<int, int> startNode = {0, 0};
+  stack<pair<int, int>> dfsStack;
 
-  while (!dfsQueue.empty()) {
-    auto [currentRow, currentCol] = dfsQueue.top();
-    dfsQueue.pop();
+  dfsStack.push(startNode);
+  openNodes.insert(startNode);
 
-    bool isVisited = visited[currentRow][currentCol] == true;
-    if (!isVisited) {
-      visited[currentRow][currentCol] = true;
+  while (!dfsStack.empty()) {
+    auto [currentRow, currentCol] = dfsStack.top();
+    dfsStack.pop();
 
-      int neighborCount = 0;
-      for (auto [deltaRow, deltaCol] : DELTAS) {
-        int nextRow = currentRow + deltaRow;
-        int nextCol = currentCol + deltaCol;
+    if (visited[currentRow][currentCol]) {
+      continue;
+    }
+    visited[currentRow][currentCol] = true;
 
-        bool isInBoundRow = nextRow >= 0 && nextRow < (int)cheeseGrid.size();
-        bool isInBoundCol = nextCol >= 0 && nextCol < (int)cheeseGrid[0].size();
+    for (auto [deltaRow, deltaCol] : DELTAS) {
+      int nextRow = currentRow + deltaRow;
+      int nextCol = currentCol + deltaCol;
 
-        bool canOpen = isInBoundRow && isInBoundCol &&
-                       (cheeseGrid[nextRow][nextCol] == true);
+      bool isInBoundRow = nextRow >= 0 && nextRow < (int)inGrid.size();
+      bool isInBoundCol = nextCol >= 0 && nextCol < (int)inGrid[0].size();
 
-        bool canVisit = canOpen && (visited[nextRow][nextCol] == false);
+      if (isInBoundRow && isInBoundCol) {
+
+        bool canVisit = (visited[nextRow][nextCol] == false);
+        bool isWall = (inGrid[nextRow][nextCol] == WALL);
 
         if (canVisit) {
-
-          dfsQueue.push({nextRow, nextCol});
-        }
-
-        if (cheeseGrid[nextRow][nextCol] == true) {
-          neighborCount++;
+          pair<int, int> nextNode = {nextRow, nextCol};
+          if (isWall) {
+            openNodes.insert(nextNode);
+            dfsStack.push(nextNode);
+          } else {
+            history[nextRow][nextCol] = hourCount;
+          }
         }
       }
-      cheeseGrid[currentRow][currentCol] = neighborCount;
     }
   }
 };
@@ -72,28 +80,31 @@ int main() {
   int rowCount = 0, colCount = 0;
   cin >> rowCount >> colCount;
 
-  vector<vector<int>> cheeseGrid(rowCount, vector<int>(colCount, 0));
-  readGrid(cheeseGrid);
+  vector<vector<bool>> inGrid(rowCount, vector<bool>(colCount, false));
+  readGrid(inGrid);
 
-  vector<int> cheeseCounts;
-  vector<vector<bool>> visited(rowCount, vector<bool>(colCount, false));
-  int totalHours = 1;
-  for (int rowIndex = 0; rowIndex < rowCount; ++rowIndex) {
-    for (int colIndex = 0; colIndex < colCount; ++colIndex) {
-      pair<int, int> startNode = {rowIndex, colIndex};
-      if (cheeseGrid[rowIndex][colIndex]) {
-        traceCheese(startNode, cheeseGrid, visited, cheeseCounts, totalHours);
-        totalHours++;
+  int hourCount = 1;
+  map<int, int> cheeseCounts;
+  set<pair<int, int>> openNodes;
+  vector<vector<int>> history(rowCount, vector<int>(colCount, 0));
+
+  while (openNodes.size() < rowCount * colCount) {
+    vector<vector<bool>> visited(rowCount, vector<bool>(colCount, false));
+    findOpenNodes(inGrid, hourCount, openNodes, visited, history);
+
+    for (int rowIndex = 0; rowIndex < rowCount; ++rowIndex) {
+      for (int colIndex = 0; colIndex < colCount; ++colIndex) {
+        if (history[rowIndex][colIndex] == hourCount) {
+          inGrid[rowIndex][colIndex] = WALL;
+          cheeseCounts[hourCount]++;
+        }
       }
     }
+    hourCount++;
   }
-  for (vector<int> row : cheeseGrid) {
-    for (int col : row) {
-      cout << col << ' ';
-    }
-    cout << '\n';
-  }
-  cout << '\n';
+
+  auto latest = cheeseCounts.rbegin();
+  cout << latest->first << '\n' << latest->second;
 
   return 0;
 }
