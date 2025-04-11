9d158bc55e51b6038e951f73097bf8cfe27ad603
MooJi <mooji.hur@gmail.com>
2025-04-11 21:29:18 +0900

    [Feature] add solved D 4179

--- section-03/D-4179/main.cpp
+++ section-03/D-4179/main.cpp
@@ -13,12 +13,14 @@ using namespace std;
 
 constexpr char WALL = '#';
 constexpr char PATH = '.';
-constexpr char JEEHUN = 'J';
+constexpr char NAME = 'J';
 constexpr char FIRE = 'F';
 
 constexpr int DIRECTIONS = 4;
 constexpr pair<int, int> DELTAS[DIRECTIONS] = {
     {-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
+constexpr int INF = 987654321;
 
 void readGrid(vector<vector<char>> &inGrid) {
   for (size_t rowIndex = 0; rowIndex < inGrid.size(); ++rowIndex) {
@@ -30,88 +32,122 @@ void readGrid(vector<vector<char>> &inGrid) {
   }
 };
 
-void moveNext(pair<int, int> &startNode,
-              vector<vector<char>> &inGrid,
-              vector<vector<bool>> &visited) {
-
-  auto [startRow, startCol] = startNode;
-
-  if (visited[startRow][startCol]) {
-    return;
-  }
-  visited[startRow][startCol] = true;
-
+void traceFire(vector<vector<char>> &inGrid, vector<vector<int>> &fireHistory) {
   queue<pair<int, int>> bfsQueue;
+
+  for (size_t rowIndex = 0; rowIndex < inGrid.size(); ++rowIndex) {
+    for (size_t colIndex = 0; colIndex < inGrid[0].size(); ++colIndex) {
+      bool isFire = inGrid[rowIndex][colIndex] == FIRE;
+      bool canVisit = fireHistory[rowIndex][colIndex] == INF;
+
+      if (isFire && canVisit) {
+
+        bfsQueue.push({rowIndex, colIndex});
+        fireHistory[rowIndex][colIndex] = 0;
+      }
+    }
+  }
 
   while (!bfsQueue.empty()) {
     auto [currentRow, currentCol] = bfsQueue.front();
-    char currentCell = inGrid[currentRow][currentCol];
     bfsQueue.pop();
 
     for (auto [deltaRow, deltaCol] : DELTAS) {
       int nextRow = currentRow + deltaRow;
       int nextCol = currentCol + deltaCol;
 
-      char nextCell = inGrid[nextRow][nextCol];
-
       bool isInBoundRow = nextRow >= 0 && nextRow < (int)inGrid.size();
       bool isInBoundCol = nextCol >= 0 && nextCol < (int)inGrid[0].size();
-      bool isSafeNext = isInBoundRow && isInBoundCol && nextCell == PATH;
-      bool canMove = isSafeNext && !visited[nextRow][nextCol];
+
+      bool isSafeNext =
+          isInBoundRow && isInBoundCol && inGrid[nextRow][nextCol] != WALL;
+
+      bool canMove = isSafeNext && fireHistory[nextRow][nextCol] == INF;
 
       if (canMove) {
-        bool needMove = currentCell == FIRE || currentCell == JEEHUN;
-        if (needMove) {
-          visited[nextRow][nextCol] = true;
-          bfsQueue.push({nextRow, nextCol});
-          if (currentCell == FIRE) {
-            inGrid[nextRow][nextCol] = FIRE;
-          }
-        }
+        fireHistory[nextRow][nextCol] = fireHistory[currentRow][currentCol] + 1;
+        bfsQueue.push({nextRow, nextCol});
       }
     }
   }
 };
 
-int main() {
+void tracePerson(vector<vector<char>> &inGrid,
+                 vector<vector<int>> &personHistory,
+                 vector<vector<int>> &fireHistory,
+                 int &timeCount) {
 
-  int rowCount = 0, colCount = 0;
-  cin >> rowCount >> colCount;
+  for (size_t rowIndex = 0; rowIndex < inGrid.size(); ++rowIndex) {
+    for (size_t colIndex = 0; colIndex < inGrid[0].size(); ++colIndex) {
+      if (inGrid[rowIndex][colIndex] == NAME) {
+        queue<pair<int, int>> bfsQueue;
+        bfsQueue.push({rowIndex, colIndex});
+        personHistory[rowIndex][colIndex] = 0;
 
-  vector<vector<char>> inGrid(rowCount, vector<char>(colCount));
-  readGrid(inGrid);
+        while (!bfsQueue.empty()) {
+          auto [currentRow, currentCol] = bfsQueue.front();
+          bfsQueue.pop();
 
-  bool isPossible = true;
-  bool isGoal = false;
-  int timeCount = 0;
+          bool isBorderRow =
+              currentRow == 0 || currentRow == (int)inGrid.size() - 1;
+          bool isBorderCol =
+              currentCol == 0 || currentCol == (int)inGrid[0].size() - 1;
 
-  while (!isGoal) {
-    for (int startRow = 0; startRow < rowCount; ++startRow) {
-      for (int startCol = 0; startCol < colCount; ++startCol) {
-        vector<vector<bool>> visitedJ(rowCount, vector<bool>(colCount, false));
-        vector<vector<bool>> visitedF(rowCount, vector<bool>(colCount, false));
+          bool isSafe = personHistory[currentRow][currentCol] <
+                        fireHistory[currentRow][currentCol];
 
-        if (inGrid[startRow][startCol] == JEEHUN) {
-          pair<int, int> startNode = {startRow, startCol};
-          moveNext(startNode, inGrid, visitedJ);
+          if ((isBorderRow || isBorderCol) && isSafe) {
+            timeCount = personHistory[currentRow][currentCol] + 1;
+            return;
+          }
+
+          for (auto [deltaRow, deltaCol] : DELTAS) {
+            int nextRow = currentRow + deltaRow;
+            int nextCol = currentCol + deltaCol;
+
+            bool isInBoundRow = nextRow >= 0 && nextRow < (int)inGrid.size();
+            bool isInBoundCol = nextCol >= 0 && nextCol < (int)inGrid[0].size();
+
+            bool isSafeNext = isInBoundRow && isInBoundCol &&
+                              inGrid[nextRow][nextCol] != WALL;
+
+            int nextTime = personHistory[currentRow][currentCol] + 1;
+            bool isBeforeFire =
+                isSafeNext && (nextTime < fireHistory[nextRow][nextCol]);
+
+            bool canMove =
+                isBeforeFire && personHistory[nextRow][nextCol] == INF;
+
+            if (canMove) {
+              personHistory[nextRow][nextCol] =
+                  personHistory[currentRow][currentCol] + 1;
+              bfsQueue.push({nextRow, nextCol});
+            }
+          }
         }
       }
     }
+  }
+}
 
-    for (int startRow = 0; startRow < rowCount; ++startRow) {
-      for (int startCol = 0; startCol < colCount; ++startCol) {
-      }
-    }
+int main() {
+  int rowCount = 0, colCount = 0;
+  cin >> rowCount >> colCount;
 
-    if (!isPossible) {
-      cout << "IMPOSSIBLE";
-      return 0;
-    }
+  vector<vector<char>> inGrid(rowCount, vector<char>(colCount, '\0'));
+  readGrid(inGrid);
 
-    if (true) {
-      isGoal = true;
-    }
-    timeCount++;
+  vector<vector<int>> fireHistory(rowCount, vector<int>(colCount, INF));
+  vector<vector<int>> personHistory(rowCount, vector<int>(colCount, INF));
+
+  traceFire(inGrid, fireHistory);
+
+  int timeCount = -1;
+  tracePerson(inGrid, personHistory, fireHistory, timeCount);
+
+  if (timeCount == -1) {
+    cout << "IMPOSSIBLE";
+    return 0;
   }
 
   cout << timeCount;
