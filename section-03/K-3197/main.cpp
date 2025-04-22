446e248a0c258724328bc448d228755178a902d7
MooJi <mooji.hur@gmail.com>
2025-04-23 03:51:12 +0900

    [Feature] add resolved K 3197

--- section-03/K-3197/main.cpp
+++ section-03/K-3197/main.cpp
@@ -13,44 +13,57 @@ using namespace std;
 
 constexpr char WATER = '.';
 constexpr char ICE = 'X';
-constexpr char BIRD = 'L';
+constexpr char SWAN = 'L';
 
 constexpr int DIRECTIONS = 4;
 constexpr pair<int, int> DELTAS[DIRECTIONS] = {
     {-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 
-void readGrid(vector<vector<char>> &inGrid,
-              vector<pair<int, int>> &birdPositions) {
-
+void readGrid(vector<vector<char>> &inGrid) {
   for (size_t rowIndex = 0; rowIndex < inGrid.size(); ++rowIndex) {
     string row;
     getline(cin >> ws, row);
+
     for (size_t colIndex = 0; colIndex < inGrid[0].size(); ++colIndex) {
       inGrid[rowIndex][colIndex] = row[colIndex];
+    }
+  }
+};
 
-      if (row[colIndex] == BIRD) {
-        birdPositions.push_back({rowIndex, colIndex});
+void initFloodFills(vector<vector<char>> &inGrid,
+                    queue<pair<int, int>> &swanQueue,
+                    vector<vector<bool>> &swanVisited,
+                    queue<pair<int, int>> &waterQueue,
+                    vector<vector<bool>> &waterVisited) {
+
+  for (size_t rowIndex = 0; rowIndex < inGrid.size(); ++rowIndex) {
+    for (size_t colIndex = 0; colIndex < inGrid[0].size(); ++colIndex) {
+      char currentCell = inGrid[rowIndex][colIndex];
+
+      bool isFirstSWAN = currentCell == SWAN && swanQueue.empty();
+      if (isFirstSWAN) {
+        swanQueue.push({rowIndex, colIndex});
+        swanVisited[rowIndex][colIndex] = true;
+      }
+
+      bool isWater = currentCell == WATER || currentCell == SWAN;
+      if (isWater) {
+        waterQueue.push({rowIndex, colIndex});
+        waterVisited[rowIndex][colIndex] = true;
       }
     }
   }
 };
 
-void meetBirds(bool &canMeet,
-               vector<vector<char>> &inGrid,
-               vector<pair<int, int>> &birdPositions,
-               vector<pair<int, int>> &icePositions) {
+void moveSwan(bool &canMeet,
+              vector<vector<char>> &inGrid,
+              queue<pair<int, int>> &swanQueue,
+              vector<vector<bool>> &swanVisited,
+              queue<pair<int, int>> &swanNextQueue) {
 
-  auto [startRow, startCol] = *birdPositions.begin();
-  queue<pair<int, int>> bfsQueue;
-  bfsQueue.push({startRow, startCol});
-
-  vector<vector<bool>> visited(inGrid.size(),
-                               vector<bool>(inGrid[0].size(), false));
-  visited[startRow][startCol] = true;
-
-  while (!bfsQueue.empty()) {
-    auto [currentRow, currentCol] = bfsQueue.front();
-    bfsQueue.pop();
+  while (!swanQueue.empty()) {
+    auto [currentRow, currentCol] = swanQueue.front();
+    swanQueue.pop();
 
     for (auto [deltaRow, deltaCol] : DELTAS) {
       int nextRow = currentRow + deltaRow;
@@ -59,25 +72,18 @@ void meetBirds(bool &canMeet,
       bool isInRow = nextRow >= 0 && nextRow < (int)inGrid.size();
       bool isInCol = nextCol >= 0 && nextCol < (int)inGrid[0].size();
 
-      bool needVisit = isInRow && isInCol && !visited[nextRow][nextCol];
-      bool canMove = needVisit && (inGrid[nextRow][nextCol] == WATER);
-      bool readyMeet = needVisit && (inGrid[nextRow][nextCol] == BIRD);
+      bool needVisit = isInRow && isInCol && !swanVisited[nextRow][nextCol];
 
       if (needVisit) {
-        switch (inGrid[nextRow][nextCol]) {
-        case ICE:
-          icePositions.push_back({nextRow, nextCol});
-          break;
-        case WATER:
-          bfsQueue.push({nextRow, nextCol});
-          visited[nextRow][nextCol] = true;
-          break;
-        case BIRD:
-          canMeet = true;
-          break;
+        swanVisited[nextRow][nextCol] = true;
+        char nextCell = inGrid[nextRow][nextCol];
 
-        default:
-          break;
+        if (nextCell == SWAN) {
+          canMeet = true;
+        } else if (nextCell == WATER) {
+          swanQueue.push({nextRow, nextCol});
+        } else if (nextCell == ICE) {
+          swanNextQueue.push({nextRow, nextCol});
         }
       }
     }
@@ -85,44 +91,68 @@ void meetBirds(bool &canMeet,
 };
 
 void meltIce(vector<vector<char>> &inGrid,
-             vector<pair<int, int>> &icePositions) {
-  for (auto [iceRow, iceCol] : icePositions) {
-    inGrid[iceRow][iceCol] = WATER;
-  }
+             queue<pair<int, int>> &waterQueue,
+             vector<vector<bool>> &waterVisited,
+             queue<pair<int, int>> &waterNextQueue) {
+  while (!waterQueue.empty()) {
+    auto [currentRow, currentCol] = waterQueue.front();
+    waterQueue.pop();
 
-  for (auto row : inGrid) {
-    for (char cell : row) {
-      cout << cell << ' ';
+    for (auto [deltaRow, deltaCol] : DELTAS) {
+      int nextRow = currentRow + deltaRow;
+      int nextCol = currentCol + deltaCol;
+
+      bool isInRow = nextRow >= 0 && nextRow < (int)inGrid.size();
+      bool isInCol = nextCol >= 0 && nextCol < (int)inGrid[0].size();
+
+      bool needVisit = isInRow && isInCol && !waterVisited[nextRow][nextCol];
+      bool needMelting = needVisit && inGrid[nextRow][nextCol] == ICE;
+
+      if (needMelting) {
+        waterNextQueue.push({nextRow, nextCol});
+        waterVisited[nextRow][nextCol] = true;
+        inGrid[nextRow][nextCol] = WATER;
+      }
     }
-    cout << '\n';
   }
-
-  cout << '\n';
 };
 
 int main() {
+
   int rowCount = 0, colCount = 0;
   cin >> rowCount >> colCount;
 
   vector<vector<char>> inGrid(rowCount, vector<char>(colCount));
-  vector<pair<int, int>> birdPositions;
+  readGrid(inGrid);
 
-  readGrid(inGrid, birdPositions);
+  queue<pair<int, int>> swanQueue;
+  vector<vector<bool>> swanVisited(rowCount, vector<bool>(colCount, false));
+
+  queue<pair<int, int>> waterQueue;
+  vector<vector<bool>> waterVisited(rowCount, vector<bool>(colCount, false));
+  initFloodFills(inGrid, swanQueue, swanVisited, waterQueue, waterVisited);
 
   bool canMeet = false;
   int dayCount = 0;
+  queue<pair<int, int>> swanNextQueue;
+  queue<pair<int, int>> waterNextQueue;
 
-  do {
+  while (true) {
+    moveSwan(canMeet, inGrid, swanQueue, swanVisited, swanNextQueue);
+    if (canMeet)
+      break;
+    meltIce(inGrid, waterQueue, waterVisited, waterNextQueue);
+
+    swap(swanQueue, swanNextQueue);
+    swap(waterQueue, waterNextQueue);
+
+    swanNextQueue = queue<pair<int, int>>();
+    waterNextQueue = queue<pair<int, int>>();
+
     dayCount++;
-    vector<pair<int, int>> icePositions;
-    meetBirds(canMeet, inGrid, birdPositions, icePositions);
-
-    if (!icePositions.empty()) {
-      meltIce(inGrid, icePositions);
-    }
-
-  } while (canMeet == false);
+  };
 
   cout << dayCount;
+
   return 0;
 }
