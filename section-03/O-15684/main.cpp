5b9e509ee763ec9ee7e3140d9074b12008805412
MooJi <mooji.hur@gmail.com>
2025-05-10 09:54:02 +0900

    [Refactor] not solved O 15684

--- section-03/O-15684/main.cpp
+++ section-03/O-15684/main.cpp
@@ -5,89 +5,94 @@
 
 #include <climits>
 #include <iostream>
+#include <utility>
 #include <vector>
 
 using namespace std;
 
-constexpr int MAX_ADDED_LINE = 3;
+constexpr int MAX_ADD_ROW = 3;
 constexpr int INVALID_RESULT = -1;
 
-int rowSize = 0, colSize = 0, maxRowCount = 0;
+int rowSize = 0, colSize = 0, presetRowSize = 0;
 
-void readLadder(vector<vector<bool>> &ladder) {
-  for (int rowIndex = 0; rowIndex < rowSize; ++rowIndex) {
-    int inRow = 0, inLeftCol = 0;
-    cin >> inRow >> inLeftCol;
-
-    ladder[inRow][inLeftCol] = true;
+void readLadder(int &presetRowSize, vector<vector<bool>> &ladder) {
+  for (int presetIndex = 0; presetIndex < presetRowSize; ++presetIndex) {
+    int rowIndex = 0, spanIndex = 0;
+    cin >> rowIndex >> spanIndex;
+    ladder[rowIndex][spanIndex] = true;
   }
 };
 
-bool isValidLadder(vector<vector<bool>> &ladder) {
-  for (int colIndex = 1; colIndex <= colSize; ++colIndex) {
-    int startCol = (int)colIndex;
-    for (int rowIndex = 1; rowIndex <= maxRowCount; ++rowIndex) {
-      if (ladder[rowIndex][startCol]) {
+bool validateLadder(vector<vector<bool>> &ladder) {
+
+  for (int spanIndex = 1; spanIndex <= colSize; ++spanIndex) {
+    int startCol = spanIndex;
+    for (int rowIndex = 1; rowIndex <= rowSize; ++rowIndex) {
+      if (ladder[rowIndex][spanIndex] == true) {
         startCol++;
-      } else if (ladder[rowIndex][startCol - 1]) {
+      }
+
+      if (ladder[rowIndex][spanIndex - 1] == true) {
         startCol--;
       }
     }
-    if (startCol != (int)colIndex) {
+    if (startCol != spanIndex) {
       return false;
     }
   }
   return true;
 };
 
-void controlLadder(int &minAddedLine,
-                   int startRow,
-                   int addedLineCount,
+void controlLadder(int &minCount,
+                   int currentCount,
+                   pair<int, int> currentPosition,
                    vector<vector<bool>> &ladder) {
 
-  bool isCountOverRange = addedLineCount > MAX_ADDED_LINE;
-  bool isCountOverMin = addedLineCount >= minAddedLine;
-
-  if (isCountOverRange || isCountOverMin) {
+  if (currentCount >= minCount || currentCount > MAX_ADD_ROW) {
     return;
   }
 
-  if (isValidLadder(ladder)) {
-    minAddedLine = min(minAddedLine, addedLineCount);
+  if (validateLadder(ladder)) {
+    minCount = min(minCount, currentCount);
     return;
   }
 
-  for (int rowIndex = startRow; rowIndex <= maxRowCount; ++rowIndex) {
-    for (int colIndex = 1; colIndex < colSize; ++colIndex) {
+  auto [currentRow, currentSpan] = currentPosition;
 
-      if (ladder[rowIndex][colIndex] || ladder[rowIndex][colIndex - 1] ||
-          ladder[rowIndex][colIndex + 1]) {
+  for (int rowIndex = currentRow; rowIndex <= rowSize; ++rowIndex) {
+
+    for (int spanIndex = 1; spanIndex < colSize; ++spanIndex) {
+      bool currentLine = ladder[rowIndex][spanIndex];
+      bool rightLine = ladder[rowIndex][spanIndex + 1];
+      bool leftLine = ladder[rowIndex][spanIndex - 1];
+
+      if (currentLine || rightLine || leftLine) {
         continue;
       }
-      ladder[rowIndex][colIndex] = true;
-      controlLadder(minAddedLine, rowIndex, addedLineCount + 1, ladder);
-
-      ladder[rowIndex][colIndex] = false;
+      ladder[rowIndex][spanIndex] = true;
+      pair<int, int> nextPosition = {rowIndex, spanIndex};
+      controlLadder(minCount, currentCount + 1, nextPosition, ladder);
+      ladder[rowIndex][spanIndex] = false;
     }
   }
 };
 
 int main() {
 
-  cin >> colSize >> rowSize >> maxRowCount;
+  cin >> colSize >> presetRowSize >> rowSize;
 
-  vector<vector<bool>> ladder(maxRowCount + 2, vector<bool>(colSize + 2, 0));
-  readLadder(ladder);
+  vector<vector<bool>> ladder(rowSize + 2, vector<bool>(colSize + 2, false));
+  readLadder(presetRowSize, ladder);
 
-  int minAddedLine = INT_MAX;
-  int startRow = 1;
-  int initAddedLineCount = 0;
-  controlLadder(minAddedLine, startRow, initAddedLineCount, ladder);
+  int minCount = INT_MAX;
+  int initCount = 0;
+  pair<int, int> startPosition = {1, 0};
+  controlLadder(minCount, initCount, startPosition, ladder);
 
-  if (minAddedLine == INT_MAX) {
+  if (minCount == INT_MAX) {
     cout << INVALID_RESULT;
   } else {
-    cout << minAddedLine;
+    cout << minCount;
   }
 
   return 0;
