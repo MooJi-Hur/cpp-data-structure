aa71a20f1255b298576444862734b28a3000914f
MooJi <mooji.hur@gmail.com>
2025-06-08 11:37:44 +0900

    [Refactor] add 04 H 2234

--- section-04/H-2234/main.cpp
+++ section-04/H-2234/main.cpp
@@ -4,6 +4,7 @@
  */
 
 #include <iostream>
+#include <map>
 #include <utility>
 #include <vector>
 
@@ -17,104 +18,113 @@ constexpr int NORTH = 2;
 constexpr int EAST = 4;
 constexpr int SOUTH = 8;
 
-vector<vector<int>> wallInfos;
+vector<vector<int>> inGrid;
 
-// out
 int roomCount = 0;
-int maxInitRoomArea = 0;
-int maxAreaExceptOneWall = 0;
+int initRoomMaxArea = 0;
+int fixedRoomMaxArea = 0;
 
 // logic
-vector<vector<bool>> visited;
-
 constexpr int DIRECTIONS = 4;
-constexpr pair<int, int> DELTAS[DIRECTIONS] = {
-    {0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+pair<int, int> DELTAS[DIRECTIONS] = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
 
-void readCells() {
-  wallInfos.resize(rowSize, vector<int>(colSize, 0));
+vector<vector<int>> roomNumbers;
+map<int, int> roomAreas;
 
-  for (auto &row : wallInfos) {
+void readGrid() {
+  inGrid.resize(rowSize, vector<int>(colSize, 0));
+
+  for (auto &row : inGrid) {
     for (auto &cell : row) {
       cin >> cell;
     }
   }
-};
+}
 
-int goNextRoom(int currentRow, int currentCol, int roomArea) {
+int calcRoom(int currentRow, int currentCol, int areaCount) {
+
+  roomNumbers[currentRow][currentCol] = roomCount;
+  roomAreas[roomCount] = areaCount;
 
   for (int deltaIndex = 0; deltaIndex < DIRECTIONS; ++deltaIndex) {
+
     auto [deltaRow, deltaCol] = DELTAS[deltaIndex];
 
     int nextRow = currentRow + deltaRow;
     int nextCol = currentCol + deltaCol;
 
     bool isOutBound =
-        nextRow < 0 || nextCol < 0 || nextRow >= rowSize || nextCol >= colSize;
-
+        nextRow < 0 || nextRow >= rowSize || nextCol < 0 || nextCol >= colSize;
     if (isOutBound) {
       continue;
     }
 
-    int hasBlock = wallInfos[currentRow][currentCol] & (1 << deltaIndex);
-    bool stopNext = hasBlock || visited[nextRow][nextCol];
-
-    if (stopNext) {
+    bool hasWall = (inGrid[currentRow][currentCol] & (1 << deltaIndex));
+    if (hasWall) {
       continue;
     }
 
-    visited[nextRow][nextCol] = true;
-    roomArea = goNextRoom(nextRow, nextCol, roomArea + 1);
-  }
-  return roomArea;
-};
+    bool isVisited = roomNumbers[nextRow][nextCol] != 0;
+    if (isVisited) {
+      continue;
+    }
 
-void searchRoom(int &maxValue) {
-  visited.clear();
-  visited.resize(rowSize, vector<bool>(colSize, false));
+    areaCount = calcRoom(nextRow, nextCol, areaCount + 1);
+  }
+  return areaCount;
+}
+
+void searchRoom() {
   for (int rowIndex = 0; rowIndex < rowSize; ++rowIndex) {
     for (int colIndex = 0; colIndex < colSize; ++colIndex) {
 
-      if (!visited[rowIndex][colIndex]) {
-        visited[rowIndex][colIndex] = 1;
-        maxValue = max(maxValue, goNextRoom(rowIndex, colIndex, 1));
+      if (roomNumbers[rowIndex][colIndex] != 0) {
+        continue;
+      }
+      roomCount++;
 
-        if (&maxValue == &maxInitRoomArea) {
-          roomCount++;
+      initRoomMaxArea = max(initRoomMaxArea, calcRoom(rowIndex, colIndex, 1));
+    }
+  }
+}
+
+void simulateRoom() {
+  for (int rowIndex = 0; rowIndex < rowSize; ++rowIndex) {
+    for (int colIndex = 0; colIndex < colSize; ++colIndex) {
+      int currentRoom = roomNumbers[rowIndex][colIndex];
+
+      if (rowIndex + 1 < rowSize) {
+        int eastRoom = roomNumbers[rowIndex + 1][colIndex];
+
+        if (currentRoom != eastRoom) {
+          fixedRoomMaxArea = max(fixedRoomMaxArea,
+                                 roomAreas[currentRoom] + roomAreas[eastRoom]);
+        }
+      }
+
+      if (colIndex + 1 < colSize) {
+        int southRoom = roomNumbers[rowIndex][colIndex + 1];
+
+        if (currentRoom != southRoom) {
+          fixedRoomMaxArea = max(fixedRoomMaxArea,
+                                 roomAreas[currentRoom] + roomAreas[southRoom]);
         }
       }
     }
   }
-};
-
-void simulateWall() {
-  for (int rowIndex = 0; rowIndex < rowSize; ++rowIndex) {
-    for (int colIndex = 0; colIndex < colSize; ++colIndex) {
-      for (int deltaIndex = 0; deltaIndex < DIRECTIONS; ++deltaIndex) {
-        int originWall = wallInfos[rowIndex][colIndex];
-
-        wallInfos[rowIndex][colIndex] = wallInfos[rowIndex][colIndex] ^=
-            (1 << deltaIndex);
-
-        searchRoom(maxAreaExceptOneWall);
-
-        wallInfos[rowIndex][colIndex] = originWall;
-      }
-    }
-  }
-};
+}
 
 int main() {
 
   cin >> colSize >> rowSize;
+  readGrid();
 
-  readCells();
+  roomNumbers.resize(rowSize, vector<int>(colSize, 0));
+  searchRoom();
 
-  searchRoom(maxInitRoomArea);
+  simulateRoom();
 
-  simulateWall();
-
-  cout << roomCount << '\n' << maxInitRoomArea << '\n' << maxAreaExceptOneWall;
+  cout << roomCount << '\n' << initRoomMaxArea << '\n' << fixedRoomMaxArea;
 
   return 0;
 }
